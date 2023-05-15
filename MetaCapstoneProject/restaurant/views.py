2a3baa010b979017a5bf2fc0641c9b5c6c053145
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-05-15 17:02:04 +0100

    urls, settings and static files

--- MetaCapstoneProject/restaurant/views.py
+++ MetaCapstoneProject/restaurant/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def about(request):
+    return HttpResponse('About page')
+
+def book(request):
+    return HttpResponse('Book page')
+
+def menu(request):
+    return HttpResponse('Menu page')
+
+def display_menu_item(request):
+    return HttpResponse('Menu item page')
