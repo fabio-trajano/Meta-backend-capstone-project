46faaba251f82f4ceecdc16a91db4a8ec58baa74
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-05-15 19:23:07 +0100

    serializers

--- MetaCapstoneProject/restaurant/urls.py
+++ MetaCapstoneProject/restaurant/urls.py
@@ -1,11 +1,18 @@
-from django.contrib import admin
-from django.urls import path
+
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
-    path('book/', views.book, name='book'),
-    path('menu/', views.menu, name='menu'),
-    path('menu/<int:pk>/', views.display_menu_item, name='menu_item'),
+    path('about/', views.about, name="about"),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('reservations/', views.reservations, name="reservations"),
+
+    # API:
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
+    path('booking/', include(router.urls)),
 ]
--- MetaCapstoneProject/restaurant/views.py
+++ MetaCapstoneProject/restaurant/views.py
@@ -1,17 +1,42 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core import serializers
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
 
-def index(request):
+from .serializers import BookingSerializer, MenuSerializer
+from .models import Booking, Menu
+
+
+# API:
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    queryset = Booking.objects.all()
+
+# Webpage:
+
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html', {})
 
 def about(request):
-    return HttpResponse('About page')
+    return render(request, 'about.html')
 
-def book(request):
-    return HttpResponse('Book page')
-
-def menu(request):
-    return HttpResponse('Menu page')
-
-def display_menu_item(request):
-    return HttpResponse('Menu item page')
+def reservations(request:HttpRequest) -> HttpResponse:
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'reservations.html',{"bookings":booking_json})
